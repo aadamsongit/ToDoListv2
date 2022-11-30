cac39abc22a0d639167732d44a66001425e0830e
Amber Adamson <88444100+aadamsongit@users.noreply.github.com>
2022-11-30 12:30:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-amber:Wershiyatsmiz5@cluster0.gr5b6qy.mongodb.net/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin-amber:<PASSWORD>@cluster0.gr5b6qy.mongodb.net/todolistDB", { useNewUrlParser: true });
 
 const itemsSchema = {
   name: String
